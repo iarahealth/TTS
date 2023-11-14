b90e05f7a2c59cd7ba09f41c57dee8c4954b110d
pprobst <pprobst@insiberia.net>
2023-11-14 14:55:07 -0300

    feat: add output format and inference temperature params

--- xtts_inference.py
+++ xtts_inference.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-#
+
+import os
 import torch
 import torchaudio
 import argparse
+
+from pydub import AudioSegment
+from typing import Tuple, List
 from TTS.tts.configs.xtts_config import XttsConfig
 from TTS.tts.models.xtts import Xtts
-from typing import Tuple, List
 
 """
 Example usage:
@@ -41,9 +44,9 @@ def perform_inference(
     model: Xtts,
     text: str,
     language: str,
+    temperature: float,
     gpt_cond_latent: torch.Tensor,
     speaker_embedding: torch.Tensor,
-    temperature: float = 0.7,
 ) -> dict:
     out = model.inference(
         text=text,
@@ -55,8 +58,15 @@ def perform_inference(
     return out
 
 
-def save_output_wav(output_path: str, waveform: torch.Tensor) -> None:
+def convert_wav_to_format(input_wav_path: str, out_format: str):
+    output_path = input_wav_path.replace(".wav", f".{out_format}")
+    audio = AudioSegment.from_wav(input_wav_path)
+    audio.export(output_path, format=out_format)
+
+
+def save_output_wav(output_path: str, waveform: torch.Tensor) -> str:
     torchaudio.save(output_path, waveform.unsqueeze(0), 24000)
+    return output_path
 
 
 def main():
@@ -69,13 +79,21 @@ def main():
     parser.add_argument("--text", required=True, help="Input text for synthesis")
     parser.add_argument("--language", help="Language for synthesis", default="pt")
     parser.add_argument("--device", choices=["cpu", "cuda"], default="cpu", help="Device for model (cpu or cuda)")
+    parser.add_argument("--format", choices=["wav", "ogg"], default="ogg", help="Output audio format (wav or ogg)")
+    parser.add_argument(
+        "--temp", type=float, default=0.7, help="Temperature for inference; the higher the more creative (default: 0.7)"
+    )
 
     args = parser.parse_args()
 
     model = load_model(args.config, args.tokenizer, args.checkpoint, device=args.device)
     gpt_cond_latent, speaker_embedding = compute_speaker_latents(model, args.speaker_ref)
-    out = perform_inference(model, args.text, args.language, gpt_cond_latent, speaker_embedding)
-    save_output_wav(args.output, torch.tensor(out["wav"]))
+    out = perform_inference(model, args.text, args.language, args.temp, gpt_cond_latent, speaker_embedding)
+
+    output_wav_path = save_output_wav(args.output + ".wav", torch.tensor(out["wav"]))
+    if args.format != "wav":
+        convert_wav_to_format(output_wav_path, args.format)
+        os.remove(output_wav_path)
 
 
 if __name__ == "__main__":
